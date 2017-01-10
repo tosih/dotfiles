0307c16b5fe27c4cd6b619d19adad24449f24e17
tosih <sohail@runnable.com>
2017-01-10 15:17:45 -0800

    Don't install openssh due to lack of keychain compatiblity

--- brew.sh
+++ brew.sh
@@ -44,7 +44,6 @@ brew install narwhal
 # Install more recent versions of some macOS tools.
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
 brew install homebrew/php/php56 --with-gmp
 
